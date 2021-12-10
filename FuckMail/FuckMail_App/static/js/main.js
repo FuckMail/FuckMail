918989e3f283963e6350f700a30e12f95cb69a43
Batya02 <magomedov.abdul20012@gmail.com>
2021-12-10 10:25:13 +0300

    delete functions

--- FuckMail/FuckMail_App/static/js/main.js
+++ FuckMail/FuckMail_App/static/js/main.js
@@ -2,19 +2,3 @@ $('#basic-addon1').keypress(function(event){
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){}
 });
-
-/*function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-function remove_message(){
-    await sleep(2000);
-    var elem = document.getElementById("message");
-    elem.parentNode.removeChild(elem);
-    return false;
-};*/
-
-function check_work(){
-    alert(true);
-    return true;
-}
