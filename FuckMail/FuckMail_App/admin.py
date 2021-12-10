aecb74f7a9a406d8e33df1431d24bf30121568d8
Batya02 <magomedov.abdul20012@gmail.com>
2021-12-10 12:50:58 +0300

    add CacheMessages model

--- FuckMail/FuckMail_App/admin.py
+++ FuckMail/FuckMail_App/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from .models import *
 
@@ -8,9 +9,19 @@ class Emails(admin.ModelAdmin):
     fields = ["address", "password", "proxy_url", "create_at"]
     list_display = ["address", "password", "check_proxy_format", "create_at"]
 
-    @admin.display(ordering='proxy_url')
+    @admin.display(ordering="proxy_url")
     def check_proxy_format(self, obj):
         return format_html(
             '<span style="background-color: green; color: white; font-size: 15px; font-weight: bold;">%s</span>' % obj.proxy_url
         if len(obj.proxy_url.split(":")) == 4 else 
         '<span style="background-color: red; color: white; font-size: 15px; font-weight: bold;">%s</span>' % obj.proxy_url)
+
+@admin.register(CacheMessages)
+class CacheMessages(admin.ModelAdmin):
+    fields = ["message_id", "address", "from_user", "subject", "date", "visual"]
+    list_display = ["address", "from_user", "subject"]
+    readonly_fields = ["visual", "payload"]
+
+    @admin.display(ordering="payload")
+    def format_to_html(self, obj):
+        return mark_safe("<h1>Genius</h1>")
