aae25a70f80d1df440b48fc74c1ca6ece79665c4
Batya02 <magomedov.abdul20012@gmail.com>
2021-12-10 10:23:57 +0300

    add api urls

--- FuckMail/FuckMail_App/urls.py
+++ FuckMail/FuckMail_App/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
@@ -6,5 +6,7 @@ urlpatterns = [
     path("message_more_info/<str:payload>", message_more_info, name="message_more_info"),
     path("profile", profile, name="profile"),
     path("logout", logout_user, name="logout"),
-    path("add_account", add_account, name="add_account")
+    path("add_account", add_account, name="add_account"),
+    path("api/", EmailsView.as_view(), name="get_emails"),
+    path("api/cachemessages/<str:mail_address>", CacheMessagesView.as_view(), name="get_cache_messages")
 ]
