dba544d0abfd3d9c3ea291dd0d5d21efd5bd490e
Batya02 <magomedov.abdul20012@gmail.com>
2021-12-18 20:29:22 +0300

    add get Cookie funtion and read message functions

--- FuckMail/FuckMail_App/static/js/main.js
+++ FuckMail/FuckMail_App/static/js/main.js
@@ -2,3 +2,51 @@ $('#basic-addon1').keypress(function(event){
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){}
 });
+
+function show_message(message_id){
+    var elem = document.getElementById(message_id);
+    elem.style.color = "#686868";
+
+    var data = new FormData();
+    data.append("message_id", message_id);
+    fetch("read_message/", {
+        method: "POST",
+        body: data,
+        contentType: 'application/json',
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+          "X-CSRFToken": getCookie("csrftoken")
+        },
+    }).then(console.log(true))
+}
+
+function getCookie(name) {
+    var cookieValue = null;
+    var i = 0;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (i; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        crossDomain: false, // obviates need for sameOrigin test
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+});
