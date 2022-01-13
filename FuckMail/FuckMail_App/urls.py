b92e8d4a8631acb76e977924040916a48c5f3485
Batya02 <magomedov.abdul20012@gmail.com>
2022-01-13 23:29:09 +0300

    padding code urls paths; add help and download

--- FuckMail/FuckMail_App/urls.py
+++ FuckMail/FuckMail_App/urls.py
@@ -7,11 +7,17 @@ urlpatterns = [
     path("profile", profile, name="profile"),
     path("logout", logout_user, name="logout"),
     path("add_account", add_account, name="add_account"),
+
     path("api/", EmailsView.as_view(), name="get_emails"),
     path("api/cachemessages/<str:mail_address>", CacheMessagesView.as_view(), name="get_cache_messages"),
+
     path("read_message/", read_message, name="add_account"),
     path("del_mail/", del_mail, name="del_mail"),
+
     path("add_few_accounts/", add_few_accounts, name="add_few_accounts"),
     path("del_all_accounts/", del_all_accounts, name="del_all_accounts"),
-    path("del_mail_from_list/", del_mail_from_list, name="del_mail_from_list")
+    path("del_mail_from_list/", del_mail_from_list, name="del_mail_from_list"),
+
+    path("help/<str:fileName>", help, name="help"),
+    path("download_help_file/", download_help_file, name="download_help_file")
 ]
