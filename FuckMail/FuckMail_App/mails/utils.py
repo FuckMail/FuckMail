33f20397c9ba8c2c0ad181b1e9bdf9d8a05b13f9
amtp1 <magomedov.abdul20012@gmail.com>
2022-01-24 22:29:21 +0300

    add new function: get_new_messages

--- FuckMail/FuckMail_App/mails/utils.py
+++ FuckMail/FuckMail_App/mails/utils.py
@@ -123,6 +123,50 @@ class MailsCore:
             _messages = CacheMessages.objects.filter(user_id=user_id, address=mail_address).order_by("date").all()
             return _messages
 
+    def get_new_messages(self):
+        _messages = dict()
+        host = self.get_host(mail_address=self.mail_address)
+        mail = imaplib.IMAP4_SSL(host)
+        mail.login(self.mail_data[0], self.mail_data[1])
+        mail.select("INBOX")
+        last_mail_date = CacheMessages.objects.filter(user_id=self.user_id, address=self.mail_address).order_by("-date")[0]
+        last_date = dt.strftime(last_mail_date.date, "%d-%b-%Y")
+        code, data = mail.search(None, '(SINCE "%s" UNSEEN)' % last_date)
+        if code == "OK" and bool(data[0].decode("utf-8")):
+            mail_ids = data[0].split()
+
+            for i in mail_ids:
+                data = mail.fetch(i.decode("utf-8"), "(RFC822)")
+                for response_part in data:
+                    arr = response_part[0]
+                    if isinstance(arr, tuple):
+                        message = email.message_from_string(str(arr[1], "utf-8"))
+                        if host == "imap.outlook.com":
+                            try:
+                                decode_message_payload = message.get_payload(decode=True).decode("utf-8")
+                            except:
+                                try:
+                                    decode_message_payload = message.get_payload(1).get_payload(decode=True).decode("utf-8")
+                                except:
+                                    decode_message_payload = None
+                        else:
+                            decode_message_payload = message.get_payload().get_payload(decode=True).decode("utf-8")
+
+                        message_id = md5(re.sub('[^0-9a-zA-Z]+', '', message["Message-Id"]).encode("utf-8")).hexdigest()
+                        is_mail = CacheMessages.objects.filter(user_id=self.user_id, address=self.mail_address, message_id=message_id)
+                        if not is_mail.exists():
+                            CacheMessages.objects.create(
+                                message_id=message_id, address=self.mail_address,
+                                from_user=self.decode_format(message["from"]), subject=self.decode_format(message["subject"]),
+                                date=self.date_format(message["date"]), payload=decode_message_payload, user_id=self.user_id
+                            )
+                            _messages["message_id"] = message_id
+                            _messages["from_user"] = self.decode_format(message["from"])
+                            _messages["subject"] = self.decode_format(message["subject"])
+                            _messages["date"] = self.date_format(message["date"])
+                            _messages["payload"] = decode_message_payload
+        return _messages
+
     def get_mail_data(self, user_id: int, mail_address: str):
         """This is function get mail data (address and password)
         :param: mail_address
@@ -185,6 +229,14 @@ class MailsCore:
         return date_format
 
     def decode_format(self, subject):
+        """This is function decode subject format
+            receive subject param and return decode subject
+        :param: subject
+        :type: str
+        :return: subject
+        :rtype: str
+        """
+
         subject = subject.split("=?UTF-8?")[0]
         if not subject:
             return "Unknow"
