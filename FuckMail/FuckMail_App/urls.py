4441246e625c4c7391fa5a54eab31c32be63e1a7
amtp1 <magomedov.abdul20012@gmail.com>
2022-02-17 20:27:47 +0300

    add class: Web and Api

--- FuckMail/FuckMail_App/urls.py
+++ FuckMail/FuckMail_App/urls.py
@@ -2,26 +2,26 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("message_more_info/<str:payload>", message_more_info, name="message_more_info"),
-    path("profile", profile, name="profile"),
-    path("logout", logout_user, name="logout"),
-    path("add_account", add_account, name="add_account"),
+    path("", Web.index, name="index"),
+    path("message_more_info/<str:payload>", Web.message_more_info, name="message_more_info"),
+    path("profile", Web.profile, name="profile"),
+    path("logout", Web.logout_user, name="logout"),
+    path("add_account", Web.add_account, name="add_account"),
 
-    path("api/auth/<str:username>/<str:password>/<str:sessionid>", AuthUserView.as_view(), name="auth_user"),
-    path("api/addresses/<str:username>", AddressesView.as_view(), name="addresses"),
-    path("api/address_data/<str:username>/<str:address>", AddressDataView.as_view(), name="address_data"),
-    path("api/show_message/<str:message_id>", render_desktop_page, name="render_desktop_page"),
-    path("api/add_address/<str:username>/<str:sessionID>/<str:address>/<str:password>/<str:proxy_url>", AddAddressView.as_view(), name="add_account_desktop"),
+    path("api/auth/<str:username>/<str:password>/<str:sessionid>", API.AuthUserView.as_view(), name="auth_user"),
+    path("api/addresses/<str:username>", API.AddressesView.as_view(), name="addresses"),
+    path("api/address_data/<str:username>/<str:address>", API.AddressDataView.as_view(), name="address_data"),
+    path("api/show_message/<str:message_id>", API.render_page, name="render_page"),
+    path("api/add_address/<str:username>/<str:sessionID>/<str:address>/<str:password>/<str:proxy_url>", API.AddAddressView.as_view(), name="add_account_desktop"),
 
-    path("read_message/", read_message, name="add_account"),
-    path("del_mail/", del_mail, name="del_mail"),
+    path("read_message/", Web.read_message, name="add_account"),
+    path("del_mail/", Web.del_mail, name="del_mail"),
 
-    path("add_few_accounts/", add_few_accounts, name="add_few_accounts"),
-    path("del_all_accounts/", del_all_accounts, name="del_all_accounts"),
-    path("del_mail_from_list/", del_mail_from_list, name="del_mail_from_list"),
-    path("get_new_messages/", get_new_messages, name="get_new_messages"),
+    path("add_few_accounts/", Web.add_few_accounts, name="add_few_accounts"),
+    path("del_all_accounts/", Web.del_all_accounts, name="del_all_accounts"),
+    path("del_mail_from_list/", Web.del_mail_from_list, name="del_mail_from_list"),
+    path("get_new_messages/", Web.get_new_messages, name="get_new_messages"),
 
-    path("help/<str:fileName>", help, name="help"),
-    path("download_help_file/", download_help_file, name="download_help_file")
+    path("help/<str:fileName>", Web.help, name="help"),
+    path("download_help_file/", Web.download_help_file, name="download_help_file")
 ]
