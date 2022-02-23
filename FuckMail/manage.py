48845527fa41b82cd39bf6c8cd43a43357d9d166
amtp1 <magomedov.abdul20012@gmail.com>
2022-02-23 21:24:18 +0300

    check config file

--- FuckMail/manage.py
+++ FuckMail/manage.py
@@ -3,19 +3,23 @@
 import os
 import sys
 
+from loguru import logger
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FuckMail.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+    if not os.path.exists(r"config.yaml"):
+        return logger.warning("Don't exists file is 'config.yaml'")
+    else:
+        """Run administrative tasks."""
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FuckMail.settings')
+        try:
+            from django.core.management import execute_from_command_line
+        except ImportError as exc:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from exc
+        execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
